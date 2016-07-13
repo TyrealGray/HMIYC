c6fb1eaa9e88bc56502d64b2d8136646912cee63
tyrealgray <tyrealgray@ymail.com>
2016-07-13 21:46:12 +0800

    implement gameplayHUD

--- HuntMeIfYouCan/Source/HuntMeIfYouCan/Private/GamePlayHUD.cpp
+++ HuntMeIfYouCan/Source/HuntMeIfYouCan/Private/GamePlayHUD.cpp
@@ -2,7 +2,17 @@
 
 #include "HuntMeIfYouCan.h"
 #include "GamePlayHUD.h"
+#include "Engine/Canvas.h"
+#include "TextureResource.h"
+#include "CanvasItem.h"
 
+AGamePlayHUD::AGamePlayHUD()
+{
 
+}
 
+void AGamePlayHUD::DrawHUD()
+{
+    Super::DrawHUD();
+}
 
--- HuntMeIfYouCan/Source/HuntMeIfYouCan/Private/GamePlayHUD.h
+++ HuntMeIfYouCan/Source/HuntMeIfYouCan/Private/GamePlayHUD.h
@@ -6,14 +6,23 @@
 #include "GamePlayHUD.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class AGamePlayHUD : public AHUD
 {
-	GENERATED_BODY()
-	
-	
-	
-	
+    GENERATED_BODY()
+
+public:
+
+    AGamePlayHUD();
+
+    /** Primary draw call for the HUD */
+    virtual void DrawHUD() override;
+
+private:
+    /** Crosshair asset pointer */
+    class UTexture2D* CrosshairTex;
+
+
 };
