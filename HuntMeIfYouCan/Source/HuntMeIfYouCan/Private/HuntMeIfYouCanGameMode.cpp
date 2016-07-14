f85f4c902358afbe4ccc9e819378d8c4b96d019b
tyrealgray <tyrealgray@ymail.com>
2016-07-14 13:40:15 +0800

    and hud to mode

--- HuntMeIfYouCan/Source/HuntMeIfYouCan/Private/HuntMeIfYouCanGameMode.cpp
+++ HuntMeIfYouCan/Source/HuntMeIfYouCan/Private/HuntMeIfYouCanGameMode.cpp
@@ -2,9 +2,10 @@
 
 #include "HuntMeIfYouCan.h"
 #include "HuntMeIfYouCanGameMode.h"
+#include "GamePlayHUD.h"
 
-AHuntMeIfYouCanGameMode::AHuntMeIfYouCanGameMode(const FObjectInitializer& objectInitializer):
-    Super(objectInitializer)
+AHuntMeIfYouCanGameMode::AHuntMeIfYouCanGameMode( const FObjectInitializer& objectInitializer ):
+    Super( objectInitializer )
 {
-
+    HUDClass = AGamePlayHUD::StaticClass();
 }
