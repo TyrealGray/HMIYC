f463f435c38d7572287c0c997360f9daca5d124e
tyrealgray <tyrealgray@ymail.com>
2016-07-14 13:34:17 +0800

    add crosshair on the screen

--- HuntMeIfYouCan/Source/HuntMeIfYouCan/Private/GamePlayHUD.cpp
+++ HuntMeIfYouCan/Source/HuntMeIfYouCan/Private/GamePlayHUD.cpp
@@ -8,11 +8,26 @@
 
 AGamePlayHUD::AGamePlayHUD()
 {
-
+    static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshiarTexObj( TEXT( "/Game/Textures/T_Crosshair" ) );
+    CrosshairTex = CrosshiarTexObj.Object;
 }
 
 void AGamePlayHUD::DrawHUD()
 {
     Super::DrawHUD();
+
+    // Draw very simple crosshair
+
+    // find center of the Canvas
+    const FVector2D Center( Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f );
+
+    // offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
+    const FVector2D CrosshairDrawPosition( ( Center.X - ( CrosshairTex->GetSurfaceWidth() * 0.5 ) ),
+                                           ( Center.Y - ( CrosshairTex->GetSurfaceHeight() * 0.5f ) ) );
+
+    // draw the crosshair
+    FCanvasTileItem TileItem( CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White );
+    TileItem.BlendMode = SE_BLEND_Translucent;
+    Canvas->DrawItem( TileItem );
 }
 
